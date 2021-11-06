953ca6fc1960482f50f7bc1005bca1001314ef9e
Yoni vidal <yonivid@gmail.com>
2021-11-06 19:41:48 +0200

    added input search

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import data from '../src/data.json'
 import { DrawButtons } from './components/drawButtons/DrawButtons';
@@ -6,8 +6,8 @@ import { DrawTable } from './components/drawTable/DrawTable';
 import { getAllCurrencies, setCategoriesList } from './redux/currencyAction';
 
 function App() {
+  const [serachResult, setSerachResult] = useState(data.data)
   const dispatch = useDispatch()
-  // dispatch(getAllCorrencies(data.data))
   const currencies = useSelector((state: any) => state.currencies)
   let categoryList = Array.from(new Set(currencies.map((c: any) => {
     return c.category
@@ -15,11 +15,17 @@ function App() {
   categoryList = categoryList.map((category: any) => {
     return category = { name: category, isActive: true }
   })
+
+  const onSearchValueChange = (event: any) => {
+    const filteredCurrencies: any = (data.data.filter((currency) => currency.name.toLowerCase().includes(event.target.value.toLowerCase())))
+    setSerachResult(filteredCurrencies)
+  }
+  // dispatch(getAllCorrencies(data.data))
   // dispatch(setCategoriesList(categoryList))
 
   useEffect(() => {
-    dispatch(getAllCurrencies(data.data))
-  }, [currencies]);
+    dispatch(getAllCurrencies(serachResult))
+  }, [serachResult]);
 
   useEffect(() => {
     dispatch(setCategoriesList(categoryList))
@@ -27,7 +33,9 @@ function App() {
 
   return (
     <div className="App">
+
       <DrawButtons />
+      <input type="text" placeholder='Search by name....' onChange={onSearchValueChange} />
       <DrawTable />
     </div>
   );
